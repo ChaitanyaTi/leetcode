0355febeb99f39e15fadeb540ad2e58367696b24
ChaitanyaTi <161628128+ChaitanyaTi@users.noreply.github.com>
2026-07-05 16:17:30 +0530

    Time: 3 ms (69.51%), Space: 102.1 MB (66.28%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int perimeter = 0;
+        
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1) {
+                    // Add 4 for the current land cell
+                    perimeter += 4;
+                    
+                    // Check if there is a land neighbor above
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                    // Check if there is a land neighbor to the left
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+};
